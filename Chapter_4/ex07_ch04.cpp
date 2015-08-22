42f5be9525f301d04fcb43f83471c6436e95ef37
lolievil <loli666evil@gmail.com>
2015-08-22 00:34:36 -0500

    Create ex07_ch04.cpp

---
+++ Chapter_4/ex07_ch04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+struct Generic_Pizza {
+  std::string brand_name;
+  double diameter;
+  double weight;    
+};
+
+int main() {
+
+  Generic_Pizza input_pizza;
+  
+  std::cout << "This pizza is for loli:\nEnter pizza company: ";
+  std::getline(std::cin, input_pizza.brand_name);
+  std::cout << "Enter pizza diameter: ";
+  std::cin >> input_pizza.diameter;
+  std::cout << "Enter pizza weight: ";
+  std::cin >> input_pizza.weight;
+  
+  std::cout << "\nInformation:\nCompany: " << input_pizza.brand_name << "\nDiameter: " << input_pizza.diameter
+    << "\nWeight: " << input_pizza.weight;
+  
+  
+return 0;
+}
